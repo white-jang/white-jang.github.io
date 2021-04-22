257a7ad663f74a96fecf829e27121f0e5fedb2b8
white-jang <galaxy__12@naver.com>
2021-04-22 14:21:17 +0900

    Todo - Insert 기능 오류 고치는 중, Remove/Toggle 기능완성

--- portfolio-app/src/components/TodoList.js
+++ portfolio-app/src/components/TodoList.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./TodoList.scss";
 import TodoListItem from "./TodoListItem";
 
-const TodoList = () => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
     </div>
   );
 };
